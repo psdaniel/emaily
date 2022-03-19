e2f2027542325b9f1c2eef1466f15a3456ce314a
psdaniel <daniel.n10@hotmail.com>
2022-03-19 10:12:12 -0300

    refactor: organize application structure

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' })
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
