6277bac0857550b1b64181efb6055d31f24e339a
psdaniel <daniel.n10@hotmail.com>
2022-03-19 18:16:41 -0300

    finished auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys')
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -8,9 +22,14 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access Token:', accessToken)
-        console.log('refresh Token:', refreshToken)
-        console.log('profile:', profile)
-
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({ googleId: profile.id }).save()
+                        .then(user => done(null, user))
+                }
+            })
     })
 );
